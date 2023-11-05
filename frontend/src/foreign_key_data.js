251317499e11ea4e70eccd53b41f3c56fcfac855
JACOB SINGER <jacobbradleysinger@gmail.com>
2023-11-05 10:26:58 -0600

    add foreign key message

--- frontend/src/foreign_key_data.js
+++ frontend/src/foreign_key_data.js
@@ -1,14 +1,15 @@
 // ======================================================
 // Foreign Key Functions
 // ======================================================
+// TODO: See if there is a way to make an app route with the foreign key
+// relationship now that you know that this exists, should not have to merge
+// two tables on the frontend
 const getProductOwner = (ownerId, productData, sellerData) => {
     const productDataCopy = [...productData];
     productDataCopy.filter(product => product.owner === ownerId);
 
     const sellerID = productDataCopy[0].owner;
     const sellerDataCopy = [...sellerData];
-
-    console.log(sellerID);
 
     let sellers = sellerDataCopy.map(seller => {
         if (seller.id === sellerID) {
