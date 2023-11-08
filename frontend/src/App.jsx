ac4656b72aa3b261ac7f52309106919d49c9d838
JACOB SINGER <jacobbradleysinger@gmail.com>
2023-11-07 19:48:41 -0600

    add capability for page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
-import { ChakraProvider, Box, Input, FormLabel, VStack, HStack, Button, Heading } from "@chakra-ui/react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import { ChakraProvider } from "@chakra-ui/react";
 import './App.css';
 import { PRODUCT_OWNER_URL, PRODUCT_URL, RENTER_URL, SELLER_URL, getInitialData } from './backend_config';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   const [renterData, setRenterData] = useState(null);
@@ -9,10 +12,6 @@ function App() {
   const [sellerData, setSellerData] = useState(null);
   const [productOwners, setProductOwners] = useState(null);
   const [productOwnerByProductId, setProductOwnerByProductId] = useState(null);
-
-  const [address, setAddress] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
 
   useEffect(() => {
     getInitialData([
@@ -24,66 +23,13 @@ function App() {
     ]);
   }, []);
 
-  const addRenter = async (event) => {
-    event.preventDefault();
-
-    await fetch(RENTER_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        first_name: firstName,
-        last_name: lastName,
-        address: address
-      })
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success: ' + JSON.stringify(data));
-      })
-      .catch((error) => {
-        console.error('Error: ' + error);
-      });
-  };
-
   return (
     <ChakraProvider>
-      <VStack alignItems="flex-start">
-        <Heading>Add Renter</Heading>
-        <Box
-          display="flex"
-        >
-          <form onSubmit={addRenter}>
-            <VStack
-              alignItems="flex-start"
-            >
-              <VStack
-                alignItems="flex-start"
-              >
-                <FormLabel>First Name</FormLabel>
-                <Input w="50vw" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
-              </VStack>
-              <VStack
-                alignItems="flex-start"
-              >
-                <FormLabel>Last Name</FormLabel>
-                <Input w="50vw" value={lastName} onChange={(event) => setLastName(event.target.value)} />
-              </VStack>
-              <VStack
-                alignItems="flex-start"
-              >
-                <FormLabel>Address</FormLabel>
-                <Input w="50vw" value={address} onChange={(event) => setAddress(event.target.value)} />
-              </VStack>
-              <HStack bottom="0" position="fixed" w="90vw">
-                <Button type="submit"> Submit </Button>
-                <Button> Clear </Button>
-              </HStack>
-            </VStack>
-          </form>
-        </Box>
-      </VStack>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
