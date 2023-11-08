8bc9b1702fbf1425d756c16824dc96eaa15fe0ea
JACOB SINGER <jacobbradleysinger@gmail.com>
2023-11-07 19:24:24 -0600

    add chakra ui to SQL project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { ChakraProvider, Box, Input, FormLabel, VStack, HStack, Button, Heading } from "@chakra-ui/react";
 import './App.css';
 import { PRODUCT_OWNER_URL, PRODUCT_URL, RENTER_URL, SELLER_URL, getInitialData } from './backend_config';
 
@@ -8,6 +9,10 @@ function App() {
   const [sellerData, setSellerData] = useState(null);
   const [productOwners, setProductOwners] = useState(null);
   const [productOwnerByProductId, setProductOwnerByProductId] = useState(null);
+
+  const [address, setAddress] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   useEffect(() => {
     getInitialData([
@@ -28,10 +33,9 @@ function App() {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        id: 4,
-        first_name: "Jacob",
-        last_name: "Singer",
-        address: "935 Alabama Rd.",
+        first_name: firstName,
+        last_name: lastName,
+        address: address
       })
     })
       .then(response => response.json())
@@ -44,133 +48,43 @@ function App() {
   };
 
   return (
-    <div>
-      <table>
-        <thead>
-          <th>
-            ID
-          </th>
-          <th>
-            First Name
-          </th>
-          <th>
-            Last Name
-          </th>
-          <th>
-            Address
-          </th>
-        </thead>
-        <tbody>
-          {renterData && renterData.map(renter => {
-            return (
-              <tr key={renter.id}>
-                <td>{renter.id}</td>
-                <td>{renter.first_name}</td>
-                <td>{renter.last_name}</td>
-                <td>{renter.address}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <table>
-        <thead>
-          <th>
-            ID
-          </th>
-          <th>
-            Description
-          </th>
-          <th>
-            Price
-          </th>
-          <th>
-            Owner
-          </th>
-        </thead>
-        <tbody>
-          {productData && productData.map(product => {
-            return (
-              <tr key={product.id}>
-                <td>{product.id}</td>
-                <td>{product.description}</td>
-                <td>{product.price}</td>
-                <td>{product.owner}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <table>
-        <thead>
-          <th>
-            ID
-          </th>
-          <th>
-            First Name
-          </th>
-          <th>
-            Last Name
-          </th>
-          <th>
-            Address
-          </th>
-        </thead>
-        <tbody>
-          {sellerData && sellerData.map(seller => {
-            return (
-              <tr key={seller.id}>
-                <td>{seller.id}</td>
-                <td>{seller.first_name}</td>
-                <td>{seller.last_name}</td>
-                <td>{seller.address}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <div>
-        ==============================================================
-      </div>
-      <div>
-        All Product Owners:
-      </div>
-      <div>
-        {productOwners && productOwners.map((productOwner, i) => {
-          return (
-            <div key={i}>
-              {productOwner.first_name} {productOwner.last_name}
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        ==============================================================
-      </div>
-      <div>
-        ==============================================================
-      </div>
-      <div>
-        Specified by ID:
-      </div>
-      <div>
-        {productOwnerByProductId && productOwnerByProductId.map((productOwner, i) => {
-          return (
-            <div key={i}>
-              {productOwner.first_name} {productOwner.last_name}
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        ==============================================================
-      </div>
-      <button
-        onClick={addRenter}
-      >
-        Create renter
-      </button>
-    </div>
+    <ChakraProvider>
+      <VStack alignItems="flex-start">
+        <Heading>Add Renter</Heading>
+        <Box
+          display="flex"
+        >
+          <form onSubmit={addRenter}>
+            <VStack
+              alignItems="flex-start"
+            >
+              <VStack
+                alignItems="flex-start"
+              >
+                <FormLabel>First Name</FormLabel>
+                <Input w="50vw" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
+              </VStack>
+              <VStack
+                alignItems="flex-start"
+              >
+                <FormLabel>Last Name</FormLabel>
+                <Input w="50vw" value={lastName} onChange={(event) => setLastName(event.target.value)} />
+              </VStack>
+              <VStack
+                alignItems="flex-start"
+              >
+                <FormLabel>Address</FormLabel>
+                <Input w="50vw" value={address} onChange={(event) => setAddress(event.target.value)} />
+              </VStack>
+              <HStack bottom="0" position="fixed" w="90vw">
+                <Button type="submit"> Submit </Button>
+                <Button> Clear </Button>
+              </HStack>
+            </VStack>
+          </form>
+        </Box>
+      </VStack>
+    </ChakraProvider>
   );
 }
 
