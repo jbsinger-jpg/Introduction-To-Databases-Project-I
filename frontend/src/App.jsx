3997853bb5b39de3372fb03ae039af9d2ae3f99d
JACOB SINGER <jacobbradleysinger@gmail.com>
2023-11-11 19:14:06 -0600

    remove test code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,11 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import './App.css';
-// import { PRODUCT_OWNER_URL, PRODUCT_URL, RENTER_URL, SELLER_URL, getInitialData } from './backend_config';
 import LandingPage from './pages/LandingPage';
 
 function App() {
-  // const [renterData, setRenterData] = useState(null);
-  // const [productData, setProductData] = useState(null);
-  // const [sellerData, setSellerData] = useState(null);
-  // const [productOwners, setProductOwners] = useState(null);
-  // const [productOwnerByProductId, setProductOwnerByProductId] = useState(null);
-
-  // useEffect(() => {
-  //   getInitialData([
-  //     { url: RENTER_URL, setData: setRenterData },
-  //     { url: SELLER_URL, setData: setSellerData },
-  //     { url: PRODUCT_URL, setData: setProductData },
-  //     { url: PRODUCT_OWNER_URL, setData: setProductOwners },
-  //     { url: PRODUCT_OWNER_URL + "/1", setData: setProductOwnerByProductId },
-  //   ]);
-  // }, []);
-
   return (
+
     <ChakraProvider>
       <BrowserRouter>
         <Routes>
