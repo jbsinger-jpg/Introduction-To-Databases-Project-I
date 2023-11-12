7bc4e889f691086979c66db9887582b6059d6309
JACOB SINGER <jacobbradleysinger@gmail.com>
2023-11-12 13:53:51 -0600

    add logic to prevent update page from being visible if no options present

--- frontend/src/pages/seller/UpdateSeller.jsx
+++ frontend/src/pages/seller/UpdateSeller.jsx
@@ -48,7 +48,6 @@ export default function UpdateSeller() {
             if (sellerData) {
                 let options = [];
                 setSellerDataIsLoaded(true);
-
                 for (let i = 0; i < sellerData.length; i++) {
                     options.push({ label: sellerData[i].first_name + " " + sellerData[i].last_name, key: sellerData[i].id, value: sellerData[i].id });
                 }
@@ -60,60 +59,70 @@ export default function UpdateSeller() {
     }, [sellerData]);
 
     return (
-        <VStack alignItems="flex-start">
-            <Heading>Update Seller</Heading>
-            <Box
-                display="flex"
-            >
-                <form onSubmit={updateSeller}>
-                    <VStack
-                        alignItems="flex-start"
+        <>
+            {sellerData && sellerData.length ?
+                <VStack alignItems="flex-start">
+                    <Heading>Update Seller</Heading>
+                    <Box
+                        display="flex"
                     >
-                        <VStack
-                            alignItems="flex-start"
-                        >
-                            <FormLabel>Seller Options</FormLabel>
-                            <Select w="50vw"
-                                value={selectedSellerOption}
-                                onChange={(event) => setSelectedSellerOption(event.target.value)}
+                        <form onSubmit={updateSeller}>
+                            <VStack
+                                alignItems="flex-start"
                             >
-                                {sellerOptions && sellerOptions.map(seller => {
-                                    return (
-                                        <option
-                                            key={seller.key}
-                                            value={seller.value}
-                                        >
-                                            {seller.label}
-                                        </option>
-                                    );
-                                })}
-                            </Select>
-                        </VStack>
-                        <VStack
-                            alignItems="flex-start"
-                        >
-                            <FormLabel>First Name</FormLabel>
-                            <Input w="50vw" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
-                        </VStack>
-                        <VStack
-                            alignItems="flex-start"
-                        >
-                            <FormLabel>Last Name</FormLabel>
-                            <Input w="50vw" value={lastName} onChange={(event) => setLastName(event.target.value)} />
-                        </VStack>
-                        <VStack
-                            alignItems="flex-start"
-                        >
-                            <FormLabel>Address</FormLabel>
-                            <Input w="50vw" value={address} onChange={(event) => setAddress(event.target.value)} />
-                        </VStack>
-                        <HStack bottom="0" position="fixed" w="90vw">
-                            <Button type="submit"> Submit </Button>
-                            <Button onClick={handleClearEntries}> Clear </Button>
-                        </HStack>
-                    </VStack>
-                </form>
-            </Box>
-        </VStack>
+                                <VStack
+                                    alignItems="flex-start"
+                                >
+                                    <FormLabel>Seller Options</FormLabel>
+                                    <Select w="50vw"
+                                        value={selectedSellerOption}
+                                        onChange={(event) => setSelectedSellerOption(event.target.value)}
+                                    >
+                                        {sellerOptions && sellerOptions.map(seller => {
+                                            return (
+                                                <option
+                                                    key={seller.key}
+                                                    value={seller.value}
+                                                >
+                                                    {seller.label}
+                                                </option>
+                                            );
+                                        })}
+                                    </Select>
+                                </VStack>
+                                <VStack
+                                    alignItems="flex-start"
+                                >
+                                    <FormLabel>First Name</FormLabel>
+                                    <Input w="50vw" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
+                                </VStack>
+                                <VStack
+                                    alignItems="flex-start"
+                                >
+                                    <FormLabel>Last Name</FormLabel>
+                                    <Input w="50vw" value={lastName} onChange={(event) => setLastName(event.target.value)} />
+                                </VStack>
+                                <VStack
+                                    alignItems="flex-start"
+                                >
+                                    <FormLabel>Address</FormLabel>
+                                    <Input w="50vw" value={address} onChange={(event) => setAddress(event.target.value)} />
+                                </VStack>
+                                <HStack bottom="0" position="fixed" w="90vw">
+                                    <Button type="submit"> Submit </Button>
+                                    <Button onClick={handleClearEntries}> Clear </Button>
+                                </HStack>
+                            </VStack>
+                        </form>
+                    </Box>
+                </VStack>
+                :
+                <VStack h="70vh" w="50vw" alignItems="center" justifyContent="center">
+                    <div>
+                        No Sellers Exist Add One!
+                    </div>
+                </VStack>
+            }
+        </>
     );
 }
